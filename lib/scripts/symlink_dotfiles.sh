3bde9748f67f89e72a1bfa3e6ea9826131a348bf
Coaxial <py@poujade.org>
2016-01-20 23:12:09 -0500

    Use portable path

--- lib/scripts/symlink_dotfiles.sh
+++ lib/scripts/symlink_dotfiles.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # Links dotfiles from the repo to ~
-ln -s {.bashrc,.gemrc,.gitconfig,.gitignore_global,.gvimrc,.irssi,.vimrc,.bash_aliases,.bash_prompt,.path} ~
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
+ln -s "$DIR"/{.bashrc,.gemrc,.gitconfig,.gitignore_global,.gvimrc,.irssi,.vimrc,.bash_aliases,.bash_prompt,.path} ~
