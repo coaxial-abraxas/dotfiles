44fa7b3913baae3b2e161aadec9e7786476cc2a1
Pierre <Coaxial@users.noreply.github.com>
2016-01-23 14:39:41 -0500

    Revert "Use failsafe settings"

--- lib/scripts/symlink_dotfiles.sh
+++ lib/scripts/symlink_dotfiles.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # Links dotfiles from the repo to ~
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
-ln -s "$DIR"/{.bashrc,.gemrc,.gitconfig,.gitignore_global,.gvimrc,.irssi,.vimrc,.bash_aliases,.bash_prompt,.path} ~
+ln -s {.bashrc,.gemrc,.gitconfig,.gitignore_global,.gvimrc,.irssi,.vimrc,.bash_aliases,.bash_prompt,.path} ~
