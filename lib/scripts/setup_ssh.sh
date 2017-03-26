af1c0ddc4f79f684f66d54ab97d5e737d3172534
Coaxial <hi@64b.it>
2017-03-26 13:50:50 -0400

    Setup SSH keys

---
+++ lib/scripts/setup_ssh.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+key_file="$HOME/.ssh/id_rsa"
+
+if [ ! -f "$key_file" ]
+then
+  printf "%s doesn't exist, generating a pair.\n" "$key_file"
+  printf "Enter your email: "
+  read -r user_email
+  ssh-keygen -t rsa -b 4096 -C "$user_email" -N "" -f "$key_file"
+fi
+
+printf "Your public key is:\n"
+cat "$key_file.pub"
+printf "\n"
+
+eval "$(ssh-agent -s)" 1>/dev/null
+ssh-add "$key_file"
