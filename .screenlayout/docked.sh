a4d8fdbee0ff5d89ef333bf2abb45a0e7db1c250
coaxial <hi@64b.it>
2018-03-07 22:46:19 -0500

    Switch xrandr layout when docked

---
+++ .screenlayout/docked.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI2 --primary --mode 1920x1080 --pos 0x420 --rotate normal --output HDMI1 --off --output LVDS1 --off --output VGA1 --off
