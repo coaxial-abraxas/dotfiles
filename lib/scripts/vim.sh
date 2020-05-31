370fdfecfc4546a3e9b661f284ce90e8071ea251
Pierre <coaxial@users.noreply.github.com>
2020-05-31 11:13:39 +0200

    Symlink UltiSnips dir (#248)

--- lib/scripts/vim.sh
+++ lib/scripts/vim.sh
@@ -6,3 +6,7 @@ sudo apt install build-essential cmake python3-dev -yqq
 
 sudo apt install vim -yqq
 vim +PlugInstall +qall
+
+# UltiSnips symlink
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
+ln -s "${DIR}/.vim/UltiSnips/" "$HOME/.vim/UltiSnips"
